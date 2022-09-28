a941cb11a9b248d3ce14486754ab7642c2b877d5
omar_hasan <omar.hasan@udplatforms.com>
2022-09-29 01:07:20 +0600

    initial setup of websocket

---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"we-chat/cmd/internal/handlers"
+
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	return mux
+}
---
+++ notes.md
@@ -0,0 +1,6 @@
+# important commands
+
+## cmd-1 (run from root directory)
+```
+go run cmd/web/*.go
+```
